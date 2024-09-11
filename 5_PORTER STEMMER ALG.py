f6d81e3a3b3f86109daa435d04407cbc149b50d3
Benicia2005 <124972494+Benicia2005@users.noreply.github.com>
2024-09-11 18:03:20 +0530

    Delete 5_PORTER STEMMER ALG.py

--- 5_PORTER STEMMER ALG.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.stem import PorterStemmer
-nltk.download('punkt')
-porter_stemmer = PorterStemmer()
-words = ["running", "jumps", "easily", "fairly", "studies", "studying", "cats", "caresses", "ponies", "happiness"]
-stemmed_words = [porter_stemmer.stem(word) for word in words]
-for original, stemmed in zip(words, stemmed_words):
-    print(f"Original: {original}, Stemmed: {stemmed}")
